831dcc836e1bfc16df9d61095232aa0ce1c4ffec
mumingv <mumingv@163.com>
2016-05-29 10:59:01 +0800

    mod file

--- buaa/0529/main_1.cpp
+++ buaa/0529/main_1.cpp
@@ -2,7 +2,59 @@
 
 using namespace std;
 
+void swap1(int i, int j) {
+    int temp;
+    temp = i;
+    i = j;
+    j = temp;
+}
+
+void swap2(int *i, int *j) {
+    int temp;
+    temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void swap3(int &i, int &j) {
+    int temp;
+    temp = i;
+    i = j;
+    j = temp;
+}
+
 int main(int argc, char *argv[]) {
     char *ptr;
     cout << "test" << endl;
+
+    int a = 3;
+    int b = 5;
+    swap1(a, b);
+    cout << "sawp1: " << "a = " << a << ", " << "b = " << b << endl;
+
+    a = 3;
+    b = 5;
+    swap2(&a, &b);
+    cout << "sawp2: " << "a = " << a << ", " << "b = " << b << endl;
+
+    a = 3;
+    b = 5;
+    swap3(a, b);
+    cout << "sawp3: " << "a = " << a << ", " << "b = " << b << endl;
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
