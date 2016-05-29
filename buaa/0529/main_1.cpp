16cfaf403cf94693c8afdaa9262a3952e2b447f9
mumingv <mumingv@163.com>
2016-05-29 11:19:21 +0800

    mod file

--- buaa/0529/main_1.cpp
+++ buaa/0529/main_1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 无效
 void swap1(int i, int j) {
     int temp;
     temp = i;
@@ -9,6 +10,7 @@ void swap1(int i, int j) {
     j = temp;
 }
 
+// 有效
 void swap2(int *i, int *j) {
     int temp;
     temp = *i;
@@ -16,8 +18,17 @@ void swap2(int *i, int *j) {
     *j = temp;
 }
 
+// 有效
 void swap3(int &i, int &j) {
     int temp;
+    temp = i;
+    i = j;
+    j = temp;
+}
+
+// 无效
+void swap4(int *i, int *j) {
+    int *temp;
     temp = i;
     i = j;
     j = temp;
@@ -42,6 +53,10 @@ int main(int argc, char *argv[]) {
     swap3(a, b);
     cout << "sawp3: " << "a = " << a << ", " << "b = " << b << endl;
 
+    a = 3;
+    b = 5;
+    swap4(&a, &b);
+    cout << "sawp4: " << "a = " << a << ", " << "b = " << b << endl;
 
 }
 
